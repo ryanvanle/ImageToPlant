8d05c93f8d22d63ab22ed57b4215bd3e82bd0637
rle253 <rle253@cs.washington.edu>
2022-08-18 23:47:25 -0400

    functionally done, need to polish

--- public/index.js
+++ public/index.js
@@ -34,7 +34,7 @@
     async function prediction() {
       let tensor = preprocessImage(image);
       let predictions = await model.predict(tensor).data();
-      console.log(predictions);
+      // console.log(predictions);
 
       let topFour = Array.from(predictions)
         .map(function (p, i) {
@@ -44,9 +44,124 @@
           }
         }).sort(function (a, b) {
           return b.probability - a.probability;
-        }).slice(0, 100);
+        }).slice(0, 4);
 
-      console.log(topFour);
+      generateResults(topFour);
+    }
+
+    function generateResults(results) {
+      console.log(results);
+
+      let imageDivs = [];
+
+      for (let i = 0; i < results.length; i++) {
+        let imageElement = gen("img");
+        let string = imageString(results[i]['className']);
+
+        if (PLANT_IMAGE[string] != null) {
+          imageElement.src = "./img/plants/" + PLANT_IMAGE[string];
+          imageElement.alt = results[i]['className']
+        } else {
+          console.log("does not exist")
+          break;
+        }
+        imageDivs.push(imageElement);
+      }
+
+      console.log(image);
+
+      let originalImageSection = generateOriginalImageSection();
+      let plantImageSection = generatePlantsSection(imageDivs, results);
+      let button = generateBackToUploadButton();
+
+      let resultsPage = id("results");
+
+      resultsPage.innerHTML = "";
+
+      resultsPage.appendChild(originalImageSection);
+      resultsPage.appendChild(plantImageSection);
+
+      switchPages();
+    }
+
+    function switchPages() {
+      id("results").classList.toggle("hidden");
+      id("upload").classList.toggle("hidden");
+    }
+
+    function generateBackToUploadButton() {
+
+      let button = gen("button");
+      button.textContent = "Back To Upload"
+
+      // add event listener here
+
+      button.addEventListener("click", switchPages);
+
+      return button;
+    }
+
+    function generateOriginalImageSection() {
+
+      let section = gen("section");
+      section.id = "original";
+
+      let div = gen("div");
+      let img = gen("img");
+      img.src = image;
+      img.alt = "user's input image";
+
+      let h2 = gen("h2");
+      h2.textContent = "Original Plant"
+
+      div.appendChild(img);
+      div.appendChild(h2);
+
+      let backButton = generateBackToUploadButton();
+
+      div.appendChild(backButton);
+
+      section.appendChild(div);
+
+      return section;
+    }
+
+    function generatePlantsSection(imageDivs, results) {
+
+      if (results.length !== 4) {
+        return null;
+      }
+
+      let plants = gen("section");
+      plants.id = "plants"
+
+      let counter = 0;
+      for (let i = 0; i < 2; i++) {
+        let section = gen("section");
+        for (let j = 0; j < 2; j++) {
+          let div = gen("div");
+          let img = imageDivs[counter];
+          let h2 = gen("h2");
+          h2.textContent = results[counter]["className"];
+          div.appendChild(img)
+          div.append(h2);
+          section.append(div);
+          counter++;
+        }
+        plants.appendChild(section)
+      }
+
+      return plants;
+
+    }
+
+
+
+    function imageString(name) {
+      let imageName = name;
+      imageName = imageName.toLowerCase();
+      imageName = imageName.replace(" ", "-")
+      return imageName;
     }
 
     function preprocessImage(image) {
@@ -71,7 +186,6 @@
         let imageElement = gen("img");
         imageElement.src = image;
         id("image-div").append(imageElement);
-        console.log(reader.result);
       });
       reader.readAsDataURL(this.files[0]);
       // image = null;
